e56bec868463d253df915461107e4202ea79348b
Ruthwik B G <113303754+RuthwikBg@users.noreply.github.com>
2023-03-16 18:36:08 -0400

    Update pm2.sh

--- pm2.sh
+++ pm2.sh
@@ -3,16 +3,6 @@
 # Change to the directory where the application code is stored
 cd /home/ec2-user/webapp
 
-touch .env
-
-# Write new environment variables to .env file
-echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" > .env
-echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> .env
-echo "AWS_REGION=${AWS_REGION}" >> .env
-echo "RDS_USERNAME=${DB_USERNAME}" >> .env
-echo "RDS_PASSWORD=${DB_PASSWORD}" >> .env
-echo "RDS_HOST=${DB_HOST}" >> .env
-echo "S3_BUCKET_NAME=${S3_BUCKET_NAME}" >> .env
 
 npm install pm2 -g
 pm2 start server.js
